ddb15b35f14cbf5789b23ab7d65a2a7a048fa72c
Leonnardo Araújo <lvictora@minsait.com>
2024-12-17 10:01:54 -0300

    [FEAT] Controle dos inputs js

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,17 +1,37 @@
 let inputValor = document.getElementById('num_valor');
+let inputEdicao = document.getElementById('num_edicao');
+let inputAnoPub = document.getElementById('num_ano_publicacao');
 
-inputValor.addEventListener('input', function () {
-        let valueValor = this.value.replace(/[^\d]/g, '');
+if(inputValor !== null){
+    inputValor.addEventListener('input', function () {
+            let valueValor = this.value.replace(/[^\d]/g, '');
 
-        var formattedValor = (valueValor.slice(0,-2).replace(/\B(?=(\d{3})+(?!\d))/g, ".")) +
-            '' + valueValor.slice(-2);
+            var formattedValor = (valueValor.slice(0, -2).replace(/\B(?=(\d{3})+(?!\d))/g, ".")) +
+                '' + valueValor.slice(-2);
 
-        formattedValor = formattedValor.slice(0,-2) + ',' + formattedValor.slice(-2);
-        this.value = formattedValor;
-    }
-);
+            formattedValor = formattedValor.slice(0, -2) + ',' + formattedValor.slice(-2);
+            this.value = formattedValor;
+        }
+    );
+}
 
-$(document).ready(function() {
+if(inputEdicao !== null){
+    inputEdicao.addEventListener('input', function () {
+            this.value = this.value.replace(/[^\d]/g, '');
+        }
+    );
+}
+
+if(inputAnoPub !== null){
+    inputAnoPub.addEventListener('input', function () {
+            this.value = this.value.replace(/[^\d]/g, '');
+        }
+    );
+}
+
+$(document).ready(function () {
     // Aplicar a máscara monetária ao campo num_valor
     $('#num_valor').mask('000.000.000,00', {reverse: true});
+    $('#num_edicao').mask('000', {reverse: false});
+    $('#num_ano_publicacao').mask('0000', {reverse: false});
 });
