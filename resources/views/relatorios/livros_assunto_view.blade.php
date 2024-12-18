436ca8d4d7b8354e56102c978a076bb8d2e5259b
Leonnardo Araújo <lvictora@minsait.com>
2024-12-18 14:44:13 -0300

    [CHORE] Tratando a view caso não haja relacionamento com livros;

--- resources/views/relatorios/livros_assunto_view.blade.php
+++ resources/views/relatorios/livros_assunto_view.blade.php
@@ -3,32 +3,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Relatório de Livros por Assunto</title>
+    <title>Livros sobre {{ $strAssunto }}</title>
     <style>
         body {
             font-family: Arial, sans-serif;
-            margin: 20px;
-        }
-        h1, h2 {
-            text-align: center;
         }
         table {
             width: 100%;
             border-collapse: collapse;
-            margin-bottom: 20px;
         }
         th, td {
-            border: 1px solid #ccc;
+            border: 1px solid #ddd;
             padding: 8px;
             text-align: left;
         }
         th {
             background-color: #f4f4f4;
-        }
-        .assunto {
-            margin-top: 20px;
-            font-size: 18px;
-            font-weight: bold;
         }
         /*Style utilizado para a quebra correta do thead para cada página necessária*/
         thead {
@@ -41,31 +31,28 @@
     </style>
 </head>
 <body>
-<h1>Relatório de Livros por Assunto</h1>
-
-@foreach ($livros as $livro)
-    <div class="assunto">Assunto: {{ $livro->assunto_nome }}</div>
+<h1>Livros sobre {{ $strAssunto }}</h1>
     <table>
         <thead>
         <tr>
             <th>Título</th>
             <th>Editora</th>
-            <th>Ano</th>
+            <th>Ano de Publicação</th>
             <th>Valor</th>
             <th>Autores</th>
         </tr>
         </thead>
         <tbody>
-        <tr>
-            <td>{{ $livro->str_titulo }}</td>
-            <td>{{ $livro->str_editora }}</td>
-            <td>{{ $livro->num_ano_publicacao }}</td>
-            <td>R$ {{ number_format($livro->num_valor, 2, ',', '.') }}</td>
-            <td>{{ $livro->autores }}</td> <!-- Exibindo os autores -->
-        </tr>
+        @foreach ($livros as $livro)
+            <tr>
+                <td>{{ $livro->str_titulo }}</td>
+                <td>{{ $livro->str_editora }}</td>
+                <td>{{ $livro->num_ano_publicacao }}</td>
+                <td>R$ {{ number_format($livro->num_valor, 2, ',', '.') }}</td>
+                <td>{{ $livro->autores }}</td> <!-- Exibindo os autores -->
+            </tr>
+        @endforeach
         </tbody>
     </table>
-@endforeach
-
 </body>
 </html>
