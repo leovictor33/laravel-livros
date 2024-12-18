e8353245186b7ea89d18fe5b5161eed747b5c703
Leonnardo Araújo <lvictora@minsait.com>
2024-12-18 15:15:43 -0300

    [CHORE] PHPDoc mapeando método ->findOrFail();

--- app/Models/Assunto.php
+++ app/Models/Assunto.php
@@ -5,6 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @method static findOrFail(mixed $codigo)
+ */
 class Assunto extends Model
 {
     use HasFactory;
